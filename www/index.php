83fe1280bf7118e70a793e8a50e07421e5a70a45
nCrom <realpano@naver.com>
2025-03-02 22:18:41 +0900

    초기 프로젝트 설정

---
+++ www/index.php
@@ -0,0 +1,9 @@
+<?php
+// 스마트팜 테스트 페이지
+echo "<h1>스마트팜 프로젝트 - PHP " . phpversion() . "</h1>";
+echo "<p>현재 시간: " . date('Y-m-d H:i:s') . "</p>";
+echo "<h2>PHP 정보</h2>";
+echo "<pre>";
+phpinfo();
+echo "</pre>";
+?>
